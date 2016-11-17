de4c63e5e265c22689160ffedc5e8df465600506
jungsangwon <top1tiger@naver.com>
2016-11-17 15:51:26 +0900

    clean solution

---
+++ [3611][Practice06]_201311311_정상원/chapter10/KeyEvent.h
@@ -0,0 +1,41 @@
+#pragma once
+#include <windows.h>
+#include <iostream>
+using namespace std;
+
+class KeyEvent
+{
+private:
+	HANDLE hln;
+	COORD KeyWhere;
+	DWORD EventCount;
+	INPUT_RECORD InRec;
+	DWORD NumRead;
+	int downkey;
+
+public:
+	KeyEvent()
+	{
+		hln = GetStdHandle(STD_INPUT_HANDLE);
+		EventCount = 1;
+	}
+
+	int getkey()
+	{
+		ReadConsoleInput(hln, &InRec, 1, &NumRead);
+		if (InRec.EventType == KEY_EVENT)
+		{
+			if (InRec.Event.KeyEvent.bKeyDown)
+			{
+				downkey = InRec.Event.KeyEvent.wVirtualKeyCode;
+				return downkey;
+			}
+			else
+			{
+				return -1;
+			}
+		}
+		return -1;
+	}
+};
+
